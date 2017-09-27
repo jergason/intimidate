1ea5763734e83f6a621032bc1f83cb8e88978c01
Jamison Dance <jergason@gmail.com>
2017-09-27 08:47:27 -0600

    add prettier and eslint

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-var fs = require('fs')
-var knox = require('knox')
-var mime = require('mime')
-var waitress = require('waitress')
+var fs = require("fs");
+var knox = require("knox");
+var mime = require("mime");
+var waitress = require("waitress");
 
 /**
  * @class Retry
@@ -17,46 +17,45 @@ var waitress = require('waitress')
  */
 function Retry(opts, s3Lib, mimeLib) {
   // Inject deps for testing
-  if (typeof s3Lib === 'undefined') {
-    s3Lib = knox
+  if (typeof s3Lib === "undefined") {
+    s3Lib = knox;
   }
-  this.knox = s3Lib
+  this.knox = s3Lib;
 
-  if (typeof mimeLib === 'undefined') {
-    mimeLib = mime
+  if (typeof mimeLib === "undefined") {
+    mimeLib = mime;
   }
-  this.mime = mimeLib
+  this.mime = mimeLib;
 
+  validateRequiredOptions(opts);
 
-  validateRequiredOptions(opts)
+  this.key = opts.key || opts.accessKeyId;
+  this.secret = opts.secret || opts.secretAccessKey;
+  this.bucket = opts.bucket;
+  this.region = opts.region || "us-west-2";
 
-  this.key = opts.key || opts.accessKeyId
-  this.secret = opts.secret || opts.secretAccessKey
-  this.bucket = opts.bucket
-  this.region = opts.region || 'us-west-2'
+  this.s3Client = this.knox.createClient(opts);
 
-  this.s3Client = this.knox.createClient(opts)
-
-  this.maxRetries = opts.maxRetries || 3
-  this.backoffInterval = opts.backoffInterval || 51
+  this.maxRetries = opts.maxRetries || 3;
+  this.backoffInterval = opts.backoffInterval || 51;
 }
 
 function validateRequiredOptions(opts) {
-  if (!opts.hasOwnProperty('key') && !opts.hasOwnProperty('accessKeyId')) {
-    throw new Error("Missing required 'key' option from opts.")
-  }
-  else if (!opts.hasOwnProperty('secret') && !opts.hasOwnProperty('secretAccessKey')) {
-    throw new Error("Missing required 'secret' option from opts.")
-  }
-  else if (!opts.hasOwnProperty('bucket')) {
-    throw new Error("Missing required 'bucket' option from opts.")
+  if (!opts.key && !opts.accessKeyId) {
+    throw new Error("Missing required 'key' option from opts.");
+  } else if (!opts.secret && !opts.secretAccessKey) {
+    throw new Error("Missing required 'secret' option from opts.");
+  } else if (!opts.bucket) {
+    throw new Error("Missing required 'bucket' option from opts.");
   }
 }
 
 Retry.prototype.calculateBackoff = function(numRetries) {
-  var randMultiplier = Math.ceil(Math.random() * (Math.pow(2, numRetries + 2) - 1))
-  return this.backoffInterval * randMultiplier
-}
+  var randMultiplier = Math.ceil(
+    Math.random() * (Math.pow(2, numRetries + 2) - 1)
+  );
+  return this.backoffInterval * randMultiplier;
+};
 
 /**
  * Upload a file at sourcePath with automatic retries and exponential backoff
@@ -67,34 +66,38 @@ Retry.prototype.calculateBackoff = function(numRetries) {
  * @param cb {Function} function(err) called when upload is done or has failed too many times
  */
 Retry.prototype.upload = function(sourcePath, destination, _headers, cb) {
-  var self = this
+  var self = this;
 
-  if (typeof _headers === 'function' && typeof cb === 'undefined') {
-    cb = _headers
-    _headers = {}
+  if (typeof _headers === "function" && typeof cb === "undefined") {
+    cb = _headers;
+    _headers = {};
   }
 
   fs.readFile(sourcePath, function(err, file) {
     if (err) {
-      return cb(err)
+      return cb(err);
     }
 
     var headers = {
-      'Content-Type': self.mime.lookup(sourcePath),
-      'Content-Length': file.length
-    }
+      "Content-Type": self.mime.lookup(sourcePath),
+      "Content-Length": file.length
+    };
 
     // Add any properties from _headers to headers.
     // Don't override Content-Type and Content-Length
     for (var prop in _headers) {
-      if (_headers.hasOwnProperty(prop) && prop.toLowerCase() != 'content-type' && prop.toLowerCase() != 'content-length') {
-        headers[prop] = _headers[prop]
+      if (
+        _headers[prop] &&
+        prop.toLowerCase() != "content-type" &&
+        prop.toLowerCase() != "content-length"
+      ) {
+        headers[prop] = _headers[prop];
       }
     }
 
-    self.uploadWithRetries(file, headers, destination, cb)
-  })
-}
+    self.uploadWithRetries(file, headers, destination, cb);
+  });
+};
 
 /**
  * Upload a buffer with accompanying headers to S3.
@@ -107,16 +110,16 @@ Retry.prototype.upload = function(sourcePath, destination, _headers, cb) {
  *   or failed too many times.
  */
 Retry.prototype.uploadBuffer = function(buffer, _headers, destination, cb) {
-  var headers = {}
+  var headers = {};
 
   for (var prop in _headers) {
-    if (_headers.hasOwnProperty(prop)) {
-      headers[prop] = _headers[prop]
+    if (_headers[prop]) {
+      headers[prop] = _headers[prop];
     }
   }
 
-  this.uploadWithRetries(buffer, headers, destination, cb)
-}
+  this.uploadWithRetries(buffer, headers, destination, cb);
+};
 
 /**
  * Upload buffers with accompanying headers to S3.
@@ -128,17 +131,17 @@ Retry.prototype.uploadBuffer = function(buffer, _headers, destination, cb) {
  *   or failed too many times.
  */
 Retry.prototype.uploadBuffers = function(buffers, cb) {
-  var self = this
-  var done = waitress(buffers.length, cb)
+  var self = this;
+  var done = waitress(buffers.length, cb);
 
   buffers.forEach(function(buffer) {
-    if (typeof buffer.headers !== 'object' || buffer.headers === null) {
-      buffer.headers = {}
+    if (typeof buffer.headers !== "object" || buffer.headers === null) {
+      buffer.headers = {};
     }
 
-    self.uploadBuffer(buffer.data, buffer.headers, buffer.dest, done)
-  })
-}
+    self.uploadBuffer(buffer.data, buffer.headers, buffer.dest, done);
+  });
+};
 
 /**
  * Upload a buffer with accompanying headers to s3. Recursively calls itself
@@ -154,70 +157,84 @@ Retry.prototype.uploadBuffers = function(buffers, cb) {
  * @param cb {Function} function(err, res) called when upload is done or has failed
  *   too many times.
  */
-Retry.prototype.uploadWithRetries = function(data, headers, destination, timesRetried, cb) {
-  var self = this
+Retry.prototype.uploadWithRetries = function(
+  data,
+  headers,
+  destination,
+  timesRetried,
+  cb
+) {
+  var self = this;
   // prevent callback from being called twice
-  var callbackCalled = false
+  var callbackCalled = false;
 
   // sometimes knox failures will give EPIPE errors after sending a non-200
   // status code, so we have to guard against recusing twice
-  var recursionScheduled = false
+  var recursionScheduled = false;
 
   // Set content type and length if they aren't included
-  headers = headers || {}
-  if (!(headers['Content-Type'] || headers['content-type'])) {
-    headers['Content-Type'] = 'application/octet-stream'
+  headers = headers || {};
+  if (!(headers["Content-Type"] || headers["content-type"])) {
+    headers["Content-Type"] = "application/octet-stream";
   }
-  if (headers['Content-Length'] == undefined || headers['content-length'] == undefined) {
-    headers['Content-Length'] = data.length
+  if (
+    headers["Content-Length"] == undefined ||
+    headers["content-length"] == undefined
+  ) {
+    headers["Content-Length"] = data.length;
   }
 
-  if (typeof timesRetried === 'function') {
-    cb = timesRetried
+  if (typeof timesRetried === "function") {
+    cb = timesRetried;
     // start at -1 so we can increment up to 0 on the first call
-    timesRetried = -1
+    timesRetried = -1;
   }
 
-  timesRetried++
+  timesRetried++;
 
   function endWithError(err) {
     if (callbackCalled) {
-      return
+      return;
     }
     if (timesRetried >= self.maxRetries) {
       if (!callbackCalled) {
-        callbackCalled = true
-        return cb(err, null, timesRetried)
+        callbackCalled = true;
+        return cb(err, null, timesRetried);
       }
-    }
-    else {
+    } else {
       if (recursionScheduled) {
-        return
+        return;
       }
-      recursionScheduled = true
-      setTimeout(self.uploadWithRetries.bind(self),
-        self.calculateBackoff(timesRetried), data, headers, destination,
-        timesRetried, cb)
+      recursionScheduled = true;
+      setTimeout(
+        self.uploadWithRetries.bind(self),
+        self.calculateBackoff(timesRetried),
+        data,
+        headers,
+        destination,
+        timesRetried,
+        cb
+      );
     }
   }
 
   function endWithResponse(res) {
     if (res.statusCode !== 200) {
-      return endWithError(new Error('Invalid status code: ' + res.statusCode))
+      return endWithError(new Error("Invalid status code: " + res.statusCode));
     }
 
     if (!callbackCalled) {
-      callbackCalled = true
-      return cb(null, res, timesRetried)
+      callbackCalled = true;
+      return cb(null, res, timesRetried);
     }
   }
 
-  var client = this.s3Client.put(destination, headers)
-  client.on('response', endWithResponse)
-  client.on('error', endWithError)
+  var client = this.s3Client.put(destination, headers);
+  client.on("response", endWithResponse);
+  client.on("error", endWithError);
 
-  client.end(data)
-}
+  client.end(data);
+};
 
 /**
  * Upload a file at sourcePath with automatic retries and exponential backoff
@@ -226,16 +243,16 @@ Retry.prototype.uploadWithRetries = function(data, headers, destination, timesRe
  * @param cb {Function} function(err) called when all uploads are done or have failed too many times
  */
 Retry.prototype.uploadFiles = function(files, cb) {
-  var self = this
-  var done = waitress(files.length, cb)
+  var self = this;
+  var done = waitress(files.length, cb);
 
   files.forEach(function(file) {
-    if(typeof file.headers === 'object' && file.headers !== null) {
-      self.upload(file.src, file.dest, file.headers, done)
+    if (typeof file.headers === "object" && file.headers !== null) {
+      self.upload(file.src, file.dest, file.headers, done);
     } else {
-      self.upload(file.src, file.dest, done)
+      self.upload(file.src, file.dest, done);
     }
-  })
-}
+  });
+};
 
-module.exports = Retry
+module.exports = Retry;
--- test/index.js
+++ test/index.js
@@ -1,186 +1,263 @@
 /* eslint-env mocha */
-var assert = require('assert')
-var path = require('path')
-var Intimidate = require('../index')
+var assert = require("assert");
+var path = require("path");
+var Intimidate = require("../index");
 
-describe('Intimidate', function() {
-  var noopKnox = {createClient: function() {}}
+describe("Intimidate", function() {
+  var noopKnox = { createClient: function() {} };
 
-  it('throws an error if required options are not passed in', function() {
-    assert.throws(function () {
-      var client = new Intimidate({})
-    })
-  })
+  it("throws an error if required options are not passed in", function() {
+    assert.throws(function() {
+      // eslint-disable-next-line no-unused-vars
+      var client = new Intimidate({});
+    });
+  });
 
-  it('does not throw an error if required options are passed in', function () {
+  it("does not throw an error if required options are passed in", function() {
     assert.doesNotThrow(function() {
-      var client = new Intimidate({key: 1, secret: 1, bucket: 1}, noopKnox)
-    })
-  })
+      // eslint-disable-next-line no-unused-vars
+      var client = new Intimidate({ key: 1, secret: 1, bucket: 1 }, noopKnox);
+    });
+  });
 
-  it('does not throw an error if alternative options are passed in', function () {
+  it("does not throw an error if alternative options are passed in", function() {
     assert.doesNotThrow(function() {
-      var client = new Intimidate({accessKeyId: 1, secretAccessKey: 1, bucket: 1}, noopKnox)
-    })
-  })
+      // eslint-disable-next-line no-unused-vars
+      var client = new Intimidate(
+        { accessKeyId: 1, secretAccessKey: 1, bucket: 1 },
+        noopKnox
+      );
+    });
+  });
 
-  describe('calculateBackoff', function() {
-    it('returns a larger backoff for larger numbers of retries', function() {
-      var client = new Intimidate({key: 1, secret: 1, bucket: 1}, noopKnox)
-      var lessRetriesBackoff = client.calculateBackoff(1)
-      var moreRetriesBackoff = client.calculateBackoff(10)
-      assert(moreRetriesBackoff > lessRetriesBackoff)
-    })
-  })
+  describe("calculateBackoff", function() {
+    it("returns a larger backoff for larger numbers of retries", function() {
+      var client = new Intimidate({ key: 1, secret: 1, bucket: 1 }, noopKnox);
+      var lessRetriesBackoff = client.calculateBackoff(1);
+      var moreRetriesBackoff = client.calculateBackoff(10);
+      assert(moreRetriesBackoff > lessRetriesBackoff);
+    });
+  });
 
   // THIS IS SPARTA
   var failKnox = {
     createClient: function() {
       return {
-        put: function(put, destination) {
+        put: function(_put, _destination) {
           return {
             eventHandlers: {},
             on: function(event, cb) {
-              this.eventHandlers[event] = cb
+              this.eventHandlers[event] = cb;
             },
-            end: function(data) {
-              this.eventHandlers['error']('AN ERROR')
+            end: function(_data) {
+              this.eventHandlers.error("AN ERROR");
             }
-          }
+          };
         }
-      }
+      };
     }
-  }
+  };
 
   var successKnox = {
     createClient: function() {
       return {
-        put: function(put, destination) {
+        put: function(_put, _destination) {
           return {
             eventHandlers: {},
             on: function(event, cb) {
-              this.eventHandlers[event] = cb
+              this.eventHandlers[event] = cb;
             },
-            end: function(data) {
-              this.eventHandlers['response']({statusCode: 200})
+            end: function(_data) {
+              this.eventHandlers["response"]({ statusCode: 200 });
             }
-          }
+          };
         }
-      }
+      };
     }
-  }
+  };
 
-  describe('upload', function() {
-    it('tries uploading to s3 until maxRetries tries or it gets the hose again', function(done) {
-      var client = new Intimidate({key: 1, secret: 1, bucket: 1, backoffInterval: 1, maxRetries:4 }, failKnox)
-      client.upload(path.join(__dirname, 'fakeFile.txt'), 'destination', function(err, res, timesRetried) {
-        assert(err)
-        assert(res == null)
-        assert.equal(timesRetried, 4)
-        done()
-      })
-    })
+  describe("upload", function() {
+    it("tries uploading to s3 until maxRetries tries or it gets the hose again", function(
+      done
+    ) {
+      var client = new Intimidate(
+        { key: 1, secret: 1, bucket: 1, backoffInterval: 1, maxRetries: 4 },
+        failKnox
+      );
+      client.upload(
+        path.join(__dirname, "fakeFile.txt"),
+        "destination",
+        function(err, res, timesRetried) {
+          assert(err);
+          assert(res == null);
+          assert.equal(timesRetried, 4);
+          done();
+        }
+      );
+    });
 
-    it('tries uploading to s3 until maxRetries tries or it gets the hose again: passing headers', function(done) {
-      var client = new Intimidate({key: 1, secret: 1, bucket: 1, backoffInterval: 1, maxRetries: 4 }, failKnox)
-      var headers = { 'Content-Type': 'application/text' }
-      client.upload(path.join(__dirname, 'fakeFile.txt'), 'destination', headers, function(err, res, timesRetried) {
-        assert(err)
-        assert(res == null)
-        assert.equal(timesRetried, 4)
-        done()
-      })
-    })
+    it("tries uploading to s3 until maxRetries tries or it gets the hose again: passing headers", function(
+      done
+    ) {
+      var client = new Intimidate(
+        { key: 1, secret: 1, bucket: 1, backoffInterval: 1, maxRetries: 4 },
+        failKnox
+      );
+      var headers = { "Content-Type": "application/text" };
+      client.upload(
+        path.join(__dirname, "fakeFile.txt"),
+        "destination",
+        headers,
+        function(err, res, timesRetried) {
+          assert(err);
+          assert(res == null);
+          assert.equal(timesRetried, 4);
+          done();
+        }
+      );
+    });
 
-    it('calls the callback with a response object if the request succeeds', function(done) {
-      var client = new Intimidate({key: 1, secret: 1, bucket: 1 }, successKnox)
-      client.upload(path.join(__dirname, 'fakeFile.txt'), 'destination', function(err, res) {
-        assert.ifError(err)
-        assert(res)
-        done()
-      })
-    })
-  })
+    it("calls the callback with a response object if the request succeeds", function(
+      done
+    ) {
+      var client = new Intimidate(
+        { key: 1, secret: 1, bucket: 1 },
+        successKnox
+      );
+      client.upload(
+        path.join(__dirname, "fakeFile.txt"),
+        "destination",
+        function(err, res) {
+          assert.ifError(err);
+          assert(res);
+          done();
+        }
+      );
+    });
+  });
 
-  it('calls the callback with a response object if the request succeeds: passing headers', function(done) {
-    var client = new Intimidate({key: 1, secret: 1, bucket: 1 }, successKnox)
-    var headers = { 'Content-Type': 'application/text' }
-    client.upload(path.join(__dirname, 'fakeFile.txt'), 'destination', headers, function(err, res) {
-      assert.ifError(err)
-      assert(res)
-      done()
-    })
-  })
-
-  describe('uploadBuffer', function() {
-    it('uploads a buffer', function(done) {
-      var data = new Buffer('Shall i compare thee to a summer\'s day?')
-      var headers = {
-        'Content-Type': 'application/text'
+  it("calls the callback with a response object if the request succeeds: passing headers", function(
+    done
+  ) {
+    var client = new Intimidate({ key: 1, secret: 1, bucket: 1 }, successKnox);
+    var headers = { "Content-Type": "application/text" };
+    client.upload(
+      path.join(__dirname, "fakeFile.txt"),
+      "destination",
+      headers,
+      function(err, res) {
+        assert.ifError(err);
+        assert(res);
+        done();
       }
+    );
+  });
 
-      var client = new Intimidate({key: 1, secret: 1, bucket: 1}, successKnox)
-      client.uploadBuffer(data, headers, 'poem.txt', function(err, res) {
-        assert.ifError(err)
-        assert(res)
-        done()
-      })
-    })
-  })
+  describe("uploadBuffer", function() {
+    it("uploads a buffer", function(done) {
+      var data = makeBuffer("Shall i compare thee to a summer's day?");
+      var headers = {
+        "Content-Type": "application/text"
+      };
 
-  describe('uploadFiles', function() {
-    it('calls the callback with a array response object if the request succeeds', function(done) {
-      var client = new Intimidate({key: 1, secret: 1, bucket: 1 }, successKnox)
-      var files = [{
-        src: path.join(__dirname, 'fakeFile.txt'),
-        dest: 'destination'
-      },{
-        src: path.join(__dirname, 'fakeFile.txt'),
-        dest: 'another_destination'
-      }]
+      var client = new Intimidate(
+        { key: 1, secret: 1, bucket: 1 },
+        successKnox
+      );
+      client.uploadBuffer(data, headers, "poem.txt", function(err, res) {
+        assert.ifError(err);
+        assert(res);
+        done();
+      });
+    });
+  });
+
+  describe("uploadFiles", function() {
+    it("calls the callback with a array response object if the request succeeds", function(
+      done
+    ) {
+      var client = new Intimidate(
+        { key: 1, secret: 1, bucket: 1 },
+        successKnox
+      );
+      var files = [
+        {
+          src: path.join(__dirname, "fakeFile.txt"),
+          dest: "destination"
+        },
+        {
+          src: path.join(__dirname, "fakeFile.txt"),
+          dest: "another_destination"
+        }
+      ];
 
       client.uploadFiles(files, function(err, res) {
-        assert.ifError(err)
-        assert(res)
-        assert(res.length == files.length)
-        done()
-      })
-    })
-    it('calls the callback with the first error ', function(done) {
-      var client = new Intimidate({key: 1, secret: 1, bucket: 1 }, successKnox)
-      var files = [{
-        src: path.join(__dirname, 'doesNotExist.txt'),
-        dest: 'destination'
-      },{
-        src: path.join(__dirname, 'fakeFile.txt'),
-        dest: 'another_destination'
-      }]
+        assert.ifError(err);
+        assert(res);
+        assert(res.length == files.length);
+        done();
+      });
+    });
+    it("calls the callback with the first error ", function(done) {
+      var client = new Intimidate(
+        { key: 1, secret: 1, bucket: 1 },
+        successKnox
+      );
+      var files = [
+        {
+          src: path.join(__dirname, "doesNotExist.txt"),
+          dest: "destination"
+        },
+        {
+          src: path.join(__dirname, "fakeFile.txt"),
+          dest: "another_destination"
+        }
+      ];
 
       client.uploadFiles(files, function(err, res) {
-        assert(err)
-        assert(res)
-        done()
-      })
-    })
-  })
+        assert(err);
+        assert(res);
+        done();
+      });
+    });
+  });
 
-  describe('uploadBuffers', function() {
-    it('calls the callback with a array response object if the request succeeds', function(done) {
-      var client = new Intimidate({key: 1, secret: 1, bucket: 1 }, successKnox)
-      var buffers = [{
-        data: new Buffer('Shall I compare thee to a summer\'s day?'),
-        dest: 'destination'
-      },{
-        data: new Buffer('When you need those uploads to back off, use intimidate'),
-        dest: 'another_destination'
-      }]
+  describe("uploadBuffers", function() {
+    it("calls the callback with a array response object if the request succeeds", function(
+      done
+    ) {
+      var client = new Intimidate(
+        { key: 1, secret: 1, bucket: 1 },
+        successKnox
+      );
+      var buffers = [
+        {
+          data: makeBuffer("Shall I compare thee to a summer's day?"),
+          dest: "destination"
+        },
+        {
+          data: makeBuffer(
+            "When you need those uploads to back off, use intimidate"
+          ),
+          dest: "another_destination"
+        }
+      ];
 
       client.uploadBuffers(buffers, function(err, res) {
-        assert.ifError(err)
-        assert(res)
-        assert(res.length == buffers.length)
-        done()
-      })
-    })
-  })
-})
+        assert.ifError(err);
+        assert(res);
+        assert(res.length == buffers.length);
+        done();
+      });
+    });
+  });
+});
+
+function makeBuffer(str) {
+  // if we have Buffer.from, use it since new Buffer is deprecated in node 6
+  if (Buffer.from) {
+    return Buffer.from(str);
+  }
+  // fall back to old behavior
+  return new Buffer(str);
+}
